b7986c2fc7bd9477f8698bc9fe25842a73b2c913
Suneel Gandham <gvsuneelkumar1981@gmail.com>
2026-06-13 03:49:39 +0530

    Initial Commit of source

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,9 +8,20 @@ import reactor.core.publisher.Mono;
 @RestController
 public class HelloController {
 
+    @Value("${app.environment:local}")
+    private String environment;
+    @Value("${app.mainframe.host:localhost}")
+    private String mainframeHost;
+
     @GetMapping("/sayHello")
     public Mono<String> sayHello() {
         return Mono.just("Hi.... Hello...!!!");
+    }
+
+    @GetMapping
+    public Mono<String> config() {
+        return Mono.just("Environment: "+environment +
+                " | Mainframe Host: "+mainframeHost);
     }
 
 
